52eb25cff08c49dba12d413a5493b5ad3f4e0993
Jealin <120001096+Jealin-9@users.noreply.github.com>
2026-06-29 15:07:49 +0800

    Fix parameter name in imu_functional_safety_diagnosis

--- IMU_Driver.h
+++ IMU_Driver.h
@@ -104,7 +104,7 @@ uint8_t imu_fault_overview(uint8_t *fault_code);    /* 错误检测概览 */
 uint8_t imu_temp_detection(uint8_t *fault_code);    /* 温度错误检测 */
 uint8_t imu_range_detection(uint8_t *fault_code);    /* 范围错误检测 */
 uint8_t imu_fault_inject(uint8_t fault_code);    /* 错误注入(用来测试) */
-uint8_t imu_functional_safety_diagnosis(SM_Alarms_Struct *SM_code);    /* 功能安全检测 */
+uint8_t imu_functional_safety_diagnosis(SM_Alarms_Struct *SM_Code);    /* 功能安全检测 */
 
 uint8_t imu_temp_comp(uint16_t gyro_tc[], uint16_t gyro_qtc[], uint16_t *gyro_to, uint16_t acc_tc[], uint16_t acc_qtc[], uint16_t *acc_to);    /* IMU温度补偿 */
 uint8_t imu_R_TempComp_Reg(uint16_t gyro_tc[], uint16_t gyro_qtc[], uint16_t *gyro_to, uint16_t acc_tc[], uint16_t acc_qtc[], uint16_t *acc_to);    /* 读取IMU温度补偿数据 */
